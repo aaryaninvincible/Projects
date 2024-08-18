3f9e8788f55cfa87e3d4a4d159dab6f58bd5eaa8
Aryan <aryanraikwar78@gmail.com>
2024-08-18 17:36:52 +0530

    updated

--- Biling_Software/billing.py
+++ Biling_Software/billing.py
@@ -242,7 +242,7 @@ def total(self):
     billarea(self)
 def intro(self):
     self.txtarea.delete(1.0,END)
-    self.txtarea.insert(END,"\tWELCOME TO SUPER MARKET\n\tPhone-No.739275410")
+    self.txtarea.insert(END,"\tWELCOME TO AARYAN MARKET\n\tPhone-No.739275410")
     self.txtarea.insert(END,f"\n\nBill no. : {self.bill_no.get()}")
     self.txtarea.insert(END,f"\nCustomer Name : {self.c_name.get()}")
     self.txtarea.insert(END,f"\nPhone No. : {self.phone.get()}")
